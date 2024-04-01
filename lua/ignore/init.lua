cb3cafd14a5443a26e7b3dcd05b238adf2c0a9c5
Charly Palliere <charly.palliere@epitech.eu>
2024-04-01 21:26:07 +0200

    sdfhfsdf

--- lua/ignore/init.lua
+++ lua/ignore/init.lua
@@ -2,20 +2,53 @@ local dkjson = require 'dkjson'
 
 local M = {}
 
+local IGNORE_C =
+{
+    "# Authored by @charlypal",
+    "# Object File",
+    "*.o",
+    "*.ko",
+    "*.obj",
+    "*.elf",
+    "",
+    "# Libraries",
+    "*.lib",
+    "*.a",
+    "*.la",
+    "*.lo",
+    "",
+    "# Executable",
+    "*.exe",
+    "*.out",
+    "*.app",
+    "*.hex",
+    "*.sh",
+    "",
+    "# Debug output",
+    "vgcore.*",
+    "gmon.out",
+    "*.gcda",
+    "*.gcno",
+    "perf.data",
+    "strace.out",
+    "*.dot",
+    "",
+    "# Subject File",
+    "*.pdf",
+    "",
+    "# EPITECH Coding Style",
+    "coding-style-reports.log",
+    "",
+    "# Binary File",
+    "%s"
+}
+
 function M.update_gitignore(arg)
-    -- Read the template.json file
-    local file = io.open('lua/ignore/template.json', 'r')
-    local content = file:read('*all')
-    file:close()
-
-    -- Parse the JSON content into a Lua table
-    local data = dkjson.decode(content)
-
     -- Open the .gitignore file for writing
     local gitignore = io.open('.gitignore', 'w')
 
     -- Iterate over the IGNORE_C table
-    for _, line in ipairs(data.IGNORE_C) do
+    for _, line in ipairs(IGNORE_C) do
         -- Replace the %s placeholder
         if line == '%s' then
             -- Replace with the provided argument or "a.out" if no argument is provided
